b39439cf4af0d87655212729f3ff610669a1befd
Tamas Nagy <thomassocska@yahoo.com>
2018-05-17 12:06:33 +0200

    Add tsv processing for the new file format and optimize file reading.

--- scripts/remove_header_bind.R
+++ scripts/remove_header_bind.R
@@ -10,11 +10,11 @@ headerless_dir <- "data/1_headerless/"
 partial_files <-
     tibble(
         # Get all files with a number at the end of filename
-        file = list.files("data/0_raw/", "_\\d.txt")) %>%
+        file = list.files("data/0_raw_new/csv", "_\\d.txt")) %>%
     separate(file, c("id", "session", "measurement", "piece"), sep = "_", remove = FALSE) %>% 
     mutate( piece = str_replace(piece, ".txt", ""),
             data = map(file,
-                ~ read_csv(paste0("data/0_raw/", .x), skip = 8, col_names = FALSE, col_types = "dd") %>% 
+                ~ read_csv(paste0("data/0_raw_new/csv/", .x), skip = 8, col_names = FALSE, col_types = "dd") %>% 
                     select(time = 1, value = 2)),
             # Extract the last timesamp from each file
             last_time = map_dbl(data, ~last(.x$time)),
@@ -27,6 +27,7 @@ partial_files <-
     nest(time, value) %>% 
     mutate(filename = str_glue("{id}_AAP_{measurement}.txt"))
 
+
 # Write the files
 walk2(partial_files$data,
       partial_files$filename,
@@ -34,19 +35,34 @@ walk2(partial_files$data,
 
 # Remove header for all the other files (to make them the same) -----------
 # Get all files that does not contain numbers at the end
-good_files <- 
-    tibble(filename = setdiff(list.files("data/0_raw/"), list.files("data/0_raw/", "\\d.txt$"))) %>% 
+# There are two formats in two directories. Read them separately, and bind them together later
+good_files_csv <- 
+    tibble(filename = setdiff(list.files("data/0_raw_new/csv/"), list.files("data/0_raw_new/csv/", "\\d.txt$"))) %>% 
     mutate(data = map(filename,
-                      ~ read_csv(str_glue("data/0_raw/{.x}"), 
+                      ~ read_csv(str_glue("data/0_raw_new/csv/{.x}"), 
                                  skip = 8, 
-                                 col_names = FALSE
-                                 # col_types = "dd"
-                                 ) #%>% 
-                          # select(time = 1, value = 2)
+                                 col_names = FALSE,
+                                 col_types = "dd") %>% 
+                          select(time = 1, value = 2)
                       )
            )
 
-# Write the files
-walk2(good_files$data,
-      good_files$filename,
+good_files_tsv <- 
+    tibble(filename = list.files("data/0_raw_new/tsv/")) %>% 
+    mutate(data = map(filename,
+                      ~ read_delim(str_glue("data/0_raw_new/tsv/{.x}"), 
+                                 skip = 8, 
+                                 delim = "\t",
+                                 locale = locale(decimal_mark = ","),
+                                 col_names = c("time", "ecg"),
+                                 col_types = "dd")
+                        )
+            )
+
+all_files <- bind_rows(good_files_csv, good_files_tsv)
+
+
+# Write the files in a unified format, without header
+walk2(all_files$data,
+      all_files$filename,
       ~ write_excel_csv(.x, path = str_glue("{headerless_dir}{.y}"), na = "", col_names = FALSE))
