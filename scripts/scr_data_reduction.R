0d4d7bb3fe6c6b25bf2123898e0d558048fef1b5
Tamas Nagy <thomassocska@yahoo.com>
2018-05-17 12:07:27 +0200

    Add SCR processing and summarizing

--- scripts/scr_data_reduction.R
+++ scripts/scr_data_reduction.R
@@ -11,7 +11,7 @@ library(hms)
 library(fuzzyjoin)
 
 source("scripts/downsample.R")
-source("scripts/near_inner_join.R")
+source("scripts/change.R")
 
 # Increase memory limit (this somehow worked to process these enormous data objects, otherwise didn't work)
 memory.limit(12000000)
@@ -19,8 +19,6 @@ memory.limit(12000000)
 # dir.create("data/3_gsr_with_markers")
 
 markers <- read_csv("data/all_markers_clean.csv", col_types = "ccici")
-
-
 
 # Read all SCR data and put to one data frame
 raw_scr <-
@@ -52,7 +50,7 @@ scr_ds <-
 write_excel_csv(scr_ds, "data/3_downsampled/scr_32hz.csv")
 scr_ds <- read_csv("data/3_downsampled/scr_32hz.csv")
 
-scr_clean <-
+scr_with_markers <-
     scr_ds %>% 
     left_join(markers, by = c("id", "session", "time")) %>% 
     group_by(file, id, session) %>% 
@@ -68,13 +66,14 @@ scr_clean <-
 
 # Create marker levels for plotting    
 marker_levels <-
-    scr_clean %>% 
+    scr_with_markers %>% 
     distinct(marker_name) %>% 
     pull()
 
 scr_sum <-
-    scr_clean %>% 
+    scr_with_markers %>% 
     group_by(file, id, session, measurement, marker_name, marker) %>% 
+    # Calculate SC positive variable
     summarise(pos_change = change(value, "positive", mean)) %>% 
     ungroup() %>% 
     mutate(marker_name =  fct_relevel(marker_name, marker_levels)) %>% 
@@ -83,17 +82,12 @@ scr_sum <-
     ungroup() %>% 
     arrange(id, session, marker_name) %>% 
     select(-file) %>% 
-    group_by(id) %>% 
-    mutate(scr_increase_std = scr_increase %>% scale() %>% as.numeric()) %>% 
     ungroup()
 
 scr_sum %>% 
-    print(n = 100)
-
-scr_sum %>% 
     mutate(role = str_extract(id, ".$")) %>% 
-    group_by(id) %>% 
-    mutate(scr_increase = scr_increase %>% scale() %>% as.numeric()) %>% 
+    # group_by(id, session) %>% 
+    # mutate(scr_increase = scr_increase %>% scale() %>% as.numeric()) %>% 
     group_by(session, role, marker_name) %>% 
     summarise(mean_increase = mean(scr_increase),
               se = sd(scr_increase)/sqrt(n())) %>% 
@@ -103,21 +97,82 @@ scr_sum %>%
         geom_linerange(aes(ymax = mean_increase + se, ymin = mean_increase - se)) +
         facet_grid(role~session, scales = "free_x")
 
+# Add order and reactivity to the table
+scr_summarised <-
+    scr_sum %>% 
+    group_by(id, session) %>% 
+    mutate(baseline = if_else(marker_name == "Baseline", scr_increase, NA_real_)) %>% 
+    fill(baseline) %>% 
+    mutate(scr_reactivity = scr_increase - baseline) %>% 
+    group_by(id) %>% 
+    mutate(scr_increase_std = scr_increase %>% scale() %>% as.numeric()) %>% 
+    arrange(id, session, -scr_increase_std) %>% 
+    group_by(id, session) %>%
+    mutate(order_scr_increase = row_number()) %>% 
+    arrange(id, session, marker_name) %>% 
+    ungroup() %>% 
+    select(-baseline)
 
-write_excel_csv(scr_sum, "data/4_summarised/scr_summarised.csv")
+write_excel_csv(scr_summarised, "data/4_summarised/scr_summarised.csv")
+
+# Calculate which task/picture yielded the largest mean for each participant for each task
+scr_largest <- 
+    scr_sum %>% 
+    arrange(id, session, -scr_increase) %>% 
+    group_by(id, session) %>%
+    slice(1) %>% 
+    rename(largest_scr_increase = marker_name) %>% 
+    select(-starts_with("scr"))
+write_excel_csv(scr_largest, "data/4_summarised/scr_largest.csv")
+    
+
+# Show which picture was associated with the largest scr increase most frequently
+scr_largest %>% 
+    mutate(role = str_extract(id, "\\D")) %>% 
+    ggplot() +
+        aes(x = largest_scr_increase) +
+        geom_bar() +
+        facet_grid(role~session, scale = "free_x")
 
 
-# BUGFIX ------------------------------------------------------------------
+# 3. Calculate an aggregate variable for tasks (AAP: 2-8 pic, 1 person and 2 person pics; Puzzle: all tasks) and baseline separately
 
-markers %>% filter(str_detect(id, "174") & session == "PUZZLE")
+# AAP dyadic pics: 3, 5, 6; 1-person pics: 2, 4, 7, 8
 
-scr_clean %>% 
-    filter(str_detect(id, "174") & session == "PUZZLE") %>% 
-    distinct(marker_name)
+aap_dyadic <- c("Picture three", "Picture five", "Picture six")
+aap_single <- c("Picture two", "Picture four", "Picture seven", "Picture eight")
 
-scr_puzzle_ <-
-scr_ds  %>% 
-    filter(session == "PUZZLE") %>% 
-    near_inner_join(markers %>% filter(session == "PUZZLE"), id_col = "id", num_col = time)
+scr_aggregated <- 
+    scr_sum %>% 
+    mutate(aggregated_marker = case_when(marker_name %in% aap_dyadic ~ "aap_dyadic",
+                                         marker_name %in% aap_single ~ "aap_single",
+                                         session == "AAP" & marker_name == "Baseline" ~ "aap_baseline",
+                                         str_detect(marker_name, "Puzzle") ~ "puzzle_task",
+                                         session == "PUZZLE" & marker_name == "Baseline" ~ "puzzle_baseline",
+                                         )) %>% 
+    drop_na(aggregated_marker) %>% 
+    group_by(id, session, aggregated_marker) %>%
+    summarise(scr_increase = mean(scr_increase, na.rm = TRUE)) %>% 
+    mutate(baseline = if_else(str_detect(aggregated_marker, "baseline"), scr_increase, NA_real_)) %>% 
+    fill(baseline) %>% 
+    mutate(scr_increase_std = scale(scr_increase),
+           scr_reactivity = scr_increase - baseline) %>% 
+    ungroup() %>% 
+    select(-baseline) %>% 
+    arrange(id, session, aggregated_marker)
 
-                  
+write_excel_csv(scr_aggregated, "data/4_summarised/scr_aggregated.csv")
+
+scr_aggregated %>% 
+    group_by(session, aggregated_marker) %>% 
+    summarise(mean_increase = mean(scr_increase_std),
+              se = sd(scr_increase_std)/sqrt(n())) %>%     
+    ggplot() +
+        aes(y = mean_increase, x = aggregated_marker) +
+        geom_col() +
+        geom_linerange(aes(ymax = mean_increase + se, ymin = mean_increase - se)) +
+        facet_grid(~session, scales = "free_x")
+
+
+
+
