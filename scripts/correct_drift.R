8affdd5cb467cfe36d3d3308f02f16ac4534d77d
nthun <thomassocska@yahoo.com>
2020-04-30 21:35:01 +0200

    Correct drift script

---
+++ scripts/correct_drift.R
@@ -0,0 +1,49 @@
+# Process ECG data (downsample, correct drift, put it inrequired format)
+
+library(tidyverse)
+
+start_rate <- 256
+end_rate <- 256
+
+all_ecg <- 
+    read_csv("all_data/clean/all_ecg.csv", col_types = "ccdd") %>% 
+    mutate(session = str_replace(session, "._(\\w+)$", "\\1"))
+
+
+# Correct drift using moving average --------------------------------------
+# First verify if the method works
+temp <-
+    all_ecg %>% 
+    filter(id == "10" & session == "emot") %>% 
+    downsample("ecg", start_rate, end_rate)
+    
+
+temp %>% 
+    mutate(rolmean = caTools::runmean(value, end_rate, align = "center"),
+           corrected = value - rolmean) %>%     
+    ggplot() +
+    aes(x = time) +
+    geom_line(aes(y = corrected)) +
+    geom_line(aes(y = rolmean), color = "blue") +
+    geom_line(aes(y = value), alpha = .5)
+
+# Do the correction by using a 1 second rolling mean
+ecg_ds <- 
+    bvp %>% 
+    group_by(id, task) %>% 
+    mutate(rolmean = caTools::runmean(bvp, end_rate, align = "center"),
+           corrected = bvp - rolmean)
+
+# Prepare for saving
+ecg_save <- 
+    ecg_ds %>% 
+    select(-time, -value, -rolmean) %>% 
+    nest() %>% 
+    mutate(to_save = map_chr(data, 
+                             ~pull(.x, corrected) %>% 
+                              paste(collapse = ",")))
+
+# Save all data
+pwalk(list(ecg_save$id, ecg_save$session, ecg_save$to_save), ~write_lines(x = ..3, path = str_glue("data/5_bvp_driftless/{..1}_{..2}_BVP.txt")))
+
+
