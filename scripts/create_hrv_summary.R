73afcf83abca530847592071323d790f25f076c6
nthun <thomassocska@yahoo.com>
2020-06-02 12:17:08 +0200

    Create summarised HRV file in tidy format

--- scripts/create_hrv_summary.R
+++ scripts/create_hrv_summary.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(vroom)
 library(janitor)
 
+theme_set(theme_light())
 # Read marker file
 markers_notime <- 
     read_csv("data/all_markers_clean.csv") %>% 
@@ -25,18 +26,50 @@ hrv_summary <-
     # Create id variables from filename
     extract(filename, 
             into = c("id", "session", "marker"), 
-            regex = "ibi_(.*)_(.*)_(.*)_hrvResults.txt", 
+            regex = "data/9_hrv_by_marker/(.*)_(.*)_(.*)_hrvResults.txt", 
             convert = TRUE) %>% 
     # Clean the hrv variable names 
     mutate(variable = make_clean_names(string = variable) %>% 
-                      str_remove("_\\d$")) %>%
+                      str_remove("_\\d+$")) %>% 
     # Add marker names
     left_join(markers_notime, by = c("id", "session", "marker")) %>% 
     pivot_wider(names_from = variable, 
                 values_from = value) %>% 
-    arrange(id, session, marker)
+    arrange(id, session, marker) %>% 
+    drop_na(marker_name)
 
 
 # Save final hrv file ---------------------------------------------------------------
 
-write_excel_csv("data/4_summarised/hrv_summary.csv")
+write_excel_csv(hrv_summary, "data/4_summarised/hrv_summary.csv")
+
+hrv_summary %>% 
+  filter(scale(rmssd) < 3) %>% 
+  ggplot() +
+  aes(x = rmssd, y = hf_abs) +
+  geom_point()
+
+hrv_summary %>% 
+  filter(scale(rmssd) < 3) %>%
+  select(id, session, rmssd, hf_abs, hf_percent) %>% 
+  pivot_longer(cols = c(rmssd, hf_abs, hf_percent),
+               names_to = "hrv",
+               values_to = "value") %>% 
+  ggplot() +
+  aes(y = value, x = hrv) +
+  geom_boxplot(outlier.alpha = .2) +
+  facet_wrap(~hrv, scales = "free")
+
+hrv_summary %>% 
+  filter(scale(rmssd) < 3) %>% 
+  select(id, session, rmssd, hf_abs, lf_abs, hf_percent, meanhr, sdnn, recordingtime_s) %>% 
+  pivot_longer(cols = -c(id, session),
+               names_to = "hrv",
+               values_to = "value") %>% 
+  ggplot() +
+  aes(x = value) +
+  geom_histogram() +
+  facet_wrap(~hrv, scales = "free")
+
+
+qplot(log(hrv_summary$hf_abs), bins = 60)
