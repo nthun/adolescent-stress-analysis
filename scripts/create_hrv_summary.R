66987885d91273a06bc9abd349c08292e8238b45
nthun <thomassocska@yahoo.com>
2021-03-18 06:13:50 +0100

    Change where outliers are removed

--- scripts/create_hrv_summary.R
+++ scripts/create_hrv_summary.R
@@ -21,22 +21,23 @@ by_marker_raw <-
 
 # Process data ----------------------------------------------------------------------
 
-hrv_summary <- 
-    by_marker_raw %>% 
-    # Create id variables from filename
-    extract(filename, 
-            into = c("id", "session", "marker"), 
-            regex = "data/9_hrv_by_marker/(.*)_(.*)_(.*)_hrvResults.txt", 
-            convert = TRUE) %>% 
-    # Clean the hrv variable names 
-    mutate(variable = make_clean_names(string = variable) %>% 
-                      str_remove("_\\d+$")) %>% 
-    # Add marker names
-    left_join(markers_notime, by = c("id", "session", "marker")) %>% 
-    pivot_wider(names_from = variable, 
-                values_from = value) %>% 
-    arrange(id, session, marker) %>% 
-    drop_na(marker_name)
+hrv_summary <-
+  by_marker_raw %>%
+  # Create id variables from filename
+  extract(
+          filename,
+          into = c("id", "session", "marker"),
+          regex = "data/9_hrv_by_marker/(.*)_(.*)_(.*)_hrvResults.txt",
+          convert = TRUE) %>%
+  # Clean the hrv variable names
+  mutate(variable = make_clean_names(string = variable) %>%
+           str_remove("_\\d+$")) %>%
+  # Add marker names
+  left_join(markers_notime, by = c("id", "session", "marker")) %>%
+  pivot_wider(names_from = variable,
+              values_from = value) %>%
+  arrange(id, session, marker) %>%
+  filter(abs(scale(rmssd)) < 3)
 
 
 # Save final hrv file ---------------------------------------------------------------
@@ -44,13 +45,11 @@ hrv_summary <-
 write_excel_csv(hrv_summary, "data/4_summarised/hrv_summary.csv")
 
 hrv_summary %>% 
-  filter(scale(rmssd) < 3) %>% 
   ggplot() +
   aes(x = rmssd, y = hf_abs) +
   geom_point()
 
 hrv_summary %>% 
-  filter(scale(rmssd) < 3) %>%
   select(id, session, rmssd, hf_abs, hf_percent) %>% 
   pivot_longer(cols = c(rmssd, hf_abs, hf_percent),
                names_to = "hrv",
